ae8dd5ed1ae68942480a9e63b69c5adf523b9102
patroclos_di <christoupat@gmail.com>
2016-05-31 22:18:51 +0300

    Update Connection manager to be Thread safe during initilization

--- src/main/java/uoa/di/ds/db/ConnectionManager.java
+++ src/main/java/uoa/di/ds/db/ConnectionManager.java
@@ -3,23 +3,28 @@ package uoa.di.ds.db;
 import com.datastax.driver.core.Cluster;
 
 public class ConnectionManager {
-	
+
 	private static ConnectionManager instance = null;
+	private static Object lock;
 	private Cluster cluster = null;
 
-	private ConnectionManager(String host){
+	private ConnectionManager(String host) {
 		cluster = Cluster.builder().addContactPoint(host).build();
 	}
-	
-	public static ConnectionManager getInstance(){
+
+	public static ConnectionManager getInstance() {
 		return instance;
 	}
-	
-	public static void init(String host){
-		instance = new ConnectionManager(host);
+
+	public static void init(String host) {
+		synchronized (lock) {
+			if (instance == null) {
+				instance = new ConnectionManager(host);
+			}
+		}
 	}
-	
-	public Cluster getCluster(){
+
+	public Cluster getCluster() {
 		return cluster;
 	}
 
