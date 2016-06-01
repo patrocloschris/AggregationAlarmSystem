ffe16319fc810aab7817ec7e9994367b62ad5495
patroclos_di <christoupat@gmail.com>
2016-06-01 22:22:55 +0300

    Updaded AggreagationCassandraBolt

--- src/main/java/uoa/di/ds/storm/bolt/AggregationCassandraBolt.java
+++ src/main/java/uoa/di/ds/storm/bolt/AggregationCassandraBolt.java
@@ -1,5 +1,7 @@
 package uoa.di.ds.storm.bolt;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.storm.task.OutputCollector;
@@ -8,28 +10,99 @@ import org.apache.storm.topology.OutputFieldsDeclarer;
 import org.apache.storm.topology.base.BaseRichBolt;
 import org.apache.storm.tuple.Tuple;
 
+import com.datastax.driver.core.BatchStatement;
+import com.datastax.driver.core.Session;
+import com.datastax.driver.core.Statement;
+import com.datastax.driver.core.querybuilder.QueryBuilder;
+
+import uoa.di.ds.db.ConnectionManager;
+import uoa.di.ds.storm.utils.Cons;
+
 public class AggregationCassandraBolt extends BaseRichBolt{
 
 	private static final long serialVersionUID = 1L;
+	
+	private String field;
     private Map conf= null;
     private TopologyContext context = null;
     private OutputCollector collector = null;
+    private String cassandraHostname = null; 
+    private boolean batchMode = false;
+    private int batchSize = 10;
+	private ArrayList<Tuple> tupleList ;
+	private Session session;
+	private String keyspace;
 	
+    public AggregationCassandraBolt(String field,String keyspace) {
+    	this.tupleList = new ArrayList<Tuple>();
+    	this.field = field;
+    	this.keyspace = keyspace;
+    }
+    
 	@Override
 	public void prepare(Map stormConf, TopologyContext context, OutputCollector collector) {
 	      this.conf = stormConf;
 	      this.context = context;
 	      this.collector = collector;
+    	  ConnectionManager.init(cassandraHostname);
+    	  session = ConnectionManager.getInstance().getCluster().connect(keyspace);
 	}
 
 	@Override
 	public void execute(Tuple input) {
-		
-		
+		if(batchMode){
+			tupleList.add(input);
+			if(batchSize == tupleList.size()){
+				insertBatchToCassandra("TODO", tupleList);
+				tupleList.clear();
+			}
+		}else{
+			insertToCassandra("TODO",input);
+		}
 	}
 
 	@Override
 	public void declareOutputFields(OutputFieldsDeclarer declarer) {
+		
+	}
+	
+	public void withHostName(String name){
+		this.cassandraHostname = name;
 	}
 
+	public void withBatchMode(boolean mode){
+		this.batchMode = mode;
+	}
+
+	public void withBatchSize(int size){
+		this.batchSize = size;
+	}
+	
+	private void insertToCassandra(String table,Tuple tuple){
+		Statement statement = QueryBuilder.insertInto(table)
+		        .value("id",tuple.getIntegerByField(Cons.TUPLE_VAR_ID))
+		        .value("name", tuple.getStringByField(Cons.TUPLE_VAR_NAME))
+		        .value("site", tuple.getStringByField(Cons.TUPLE_VAR_SITE))
+		        .value("eventTime", tuple.getLongByField(Cons.TUPLE_VAR_EVENTTIME))
+		        .value("value", tuple.getFloatByField(Cons.TUPLE_VAR_VALUE))
+		        .value("duration", tuple.getIntegerByField(Cons.TUPLE_VAR_DURATION));
+		session.execute(statement);		
+	}
+
+	private void insertBatchToCassandra(String table,List<Tuple> tuples){
+		
+		BatchStatement batch = new BatchStatement();
+
+		for(Tuple tuple: tuples){
+			Statement statement = QueryBuilder.insertInto(table)
+			        .value("id",tuple.getIntegerByField(Cons.TUPLE_VAR_ID))
+			        .value("name", tuple.getStringByField(Cons.TUPLE_VAR_NAME))
+			        .value("site", tuple.getStringByField(Cons.TUPLE_VAR_SITE))
+			        .value("eventTime", tuple.getLongByField(Cons.TUPLE_VAR_EVENTTIME))
+			        .value("value", tuple.getFloatByField(Cons.TUPLE_VAR_VALUE))
+			        .value("duration", tuple.getIntegerByField(Cons.TUPLE_VAR_DURATION));
+			batch.add(statement);
+		}
+		session.equals(batch);
+	}
 }
--- src/main/java/uoa/di/ds/storm/utils/Cons.java
+++ src/main/java/uoa/di/ds/storm/utils/Cons.java
@@ -18,7 +18,7 @@ public class Cons {
 	public static final String CASSANDRA_PORT= "cassandra.port";
 	public static final String CASSANDRA_R_KEYSPACE= "cassandra.rules.keyspace";
 	public static final String CASSANDRA_R_TABLE= "cassandra.rules.table";
-
+	public static final String CASSANDRA_S_KEYSPACE= "cassandra.statistics.keyspace";
 	
 	public static final String TUPLE_VAR_MSG = "message";
 	public static final String TUPLE_VAR_KEY = "key";
@@ -30,8 +30,9 @@ public class Cons {
 	public static final String TUPLE_VAR_NAME = "name";
 	public static final String TUPLE_VAR_SITE = "site";
 	public static final String TUPLE_VAR_TEMPERATURE = "temperature";
-
-	
+	public static final String TUPLE_VAR_EVENTTIME = "eventTime";
+	public static final String TUPLE_VAR_DURATION = "duration";
+	public static final String TUPLE_VAR_VALUE = "value";
 	
 	public static final String LOCAL_ADDRS = "127.0.0.1";
 	
