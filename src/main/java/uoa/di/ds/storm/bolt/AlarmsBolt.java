3524c93e8cce31c0ee10d1b662899100ce8c91ac
eangelog <vmanos81@gmail.com>
2016-06-07 18:00:38 +0300

    Initial version of alarmsBolt

--- src/main/java/uoa/di/ds/storm/bolt/AlarmsBolt.java
+++ src/main/java/uoa/di/ds/storm/bolt/AlarmsBolt.java
@@ -1,7 +1,11 @@
 package uoa.di.ds.storm.bolt;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 import org.apache.storm.task.OutputCollector;
 import org.apache.storm.task.TopologyContext;
@@ -9,6 +13,7 @@ import org.apache.storm.topology.OutputFieldsDeclarer;
 import org.apache.storm.topology.base.BaseRichBolt;
 import org.apache.storm.tuple.Fields;
 import org.apache.storm.tuple.Tuple;
+import org.apache.storm.tuple.Values;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -33,6 +38,7 @@ public class AlarmsBolt extends BaseRichBolt{
 	private Session session;
 	private String keyspace;
 	private ArrayList<String> rulesList;
+	private Map<String , HashSet<Integer>> active_notifs;
 
 //	public static final String CASSANDRA_A_KEYSPACE = "cassandra.alarms.keyspace";
 //	public static final String CASSANDRA_AM_TABLE = "cassandra.alarms.alarms_meta";       // Is that correct?
@@ -47,6 +53,9 @@ public class AlarmsBolt extends BaseRichBolt{
 	      this.conf = stormConf;
 	      this.context = context;
 	      this.collector = collector;
+	      this.active_notifs = new HashMap<>();
+
+	      
     	  ConnectionManager.init(cassandraHostname, clusterName);
     	  session = ConnectionManager.getInstance().getCluster().connect(keyspace);
     	  LOG.info("Preparing alarms bolt....Connection for DB was=[{}]",session.getLoggedKeyspace());
@@ -57,11 +66,12 @@ public class AlarmsBolt extends BaseRichBolt{
 
     		//e.g. rule: field = "cpu" / rule=" >" / value= "90"
 			String field = row.getString("field") == null ? "field" : row.getString("field");
+			String operation = row.getString("operation") == null ? "operation" : row.getString("operation");
 			String rule = row.getString("rule") == null ? "rule" : row.getString("rule");
 			int value = row.getInt("value") == 0 ? 10 : row.getInt("value");
 			
-			String alarm_rule_tuple = field+","+rule+","+Integer.toString(value);
-			rulesList.add(alarm_rule_tuple);										//Each rule in the arraylist has the form: field,rule,value
+			String alarm_rule_tuple = field+","+operation+","+rule+","+Integer.toString(value);
+			rulesList.add(alarm_rule_tuple);										//Each rule in the arraylist has the form: field,operation,rule,value
 		}
 	}
 	
@@ -82,20 +92,107 @@ public class AlarmsBolt extends BaseRichBolt{
 			String[] tokens = alarm_rule.split(",");
 			int j=0;
 			
-			String field =  tokens[0];
-			String rule  =  tokens[1];
-			String value =  tokens[2];
+			String field 	 = tokens[0];
+			String operation = tokens[1];
+			String rule  	 = tokens[2];
+			String value 	 = tokens[3];
 			
 			
 			if (field.equals(input.getStringByField(Cons.TUPLE_VAR_FIELD))) { // Field match
-		
+				if (operation.equals(input.getStringByField(Cons.TUPLE_VAR_OPER))) {
+					
+					if (rule.equals(">")) {
+						if (input.getIntegerByField(Cons.TUPLE_VAR_VALUE) > Integer.parseInt(value)) {
+							Integer notif = generateNotifId(field, operation, rule);
+							String add_text = new String(field+ " " + operation+"("+Integer.toString(input.getIntegerByField(Cons.TUPLE_VAR_VALUE))+")" + " exceeded allowed limit of "+ value);
+							generateActiveAlarm(input.getStringByField(Cons.TUPLE_VAR_NAME), notif, add_text, input.getLongByField(Cons.TUPLE_VAR_EVENTTIME));
+						}
+						else {
+							Integer notif = generateNotifId(field, operation, rule);
+							String add_text = new String(field+ " " + operation+"("+Integer.toString(input.getIntegerByField(Cons.TUPLE_VAR_VALUE))+")" + " is now below limit of "+ value);
+							generateClearAlarm(input.getStringByField(Cons.TUPLE_VAR_NAME), notif, add_text, input.getLongByField(Cons.TUPLE_VAR_EVENTTIME));
+					
+						}
+					}
+					else if (rule.equals("<")) {
+						if (input.getIntegerByField(Cons.TUPLE_VAR_VALUE) < Integer.parseInt(value)) {
+							Integer notif = generateNotifId(field, operation, rule);
+							String add_text = new String(field+ " " + operation+"("+Integer.toString(input.getIntegerByField(Cons.TUPLE_VAR_VALUE))+")" + " descended below allowed limit of "+ value);
+							generateActiveAlarm(input.getStringByField(Cons.TUPLE_VAR_NAME), notif, add_text, input.getLongByField(Cons.TUPLE_VAR_EVENTTIME));
+						}
+						else {
+							Integer notif = generateNotifId(field, operation, rule);
+							String add_text = new String(field+ " " + operation+"("+Integer.toString(input.getIntegerByField(Cons.TUPLE_VAR_VALUE))+")" + " is now above limit of "+ value);
+							generateClearAlarm(input.getStringByField(Cons.TUPLE_VAR_NAME), notif, add_text, input.getLongByField(Cons.TUPLE_VAR_EVENTTIME));
+						}
+					}
+				/*	else if (rule.equals(">=")) {
+						if (input.getIntegerByField(Cons.TUPLE_VAR_VALUE) >= Integer.parseInt(value))
+							generateActiveAlarm();
+						else
+							generateClearAlarm();
+					}
+					else if (rule.equals("<=")) {
+						if (input.getIntegerByField(Cons.TUPLE_VAR_VALUE) <= Integer.parseInt(value))
+							generateActiveAlarm();
+						else
+							generateClearAlarm();
+					}						
+					else if (rule.equals("==")) {
+						if (input.getIntegerByField(Cons.TUPLE_VAR_VALUE) == Integer.parseInt(value))
+							generateActiveAlarm();
+						else
+							generateClearAlarm();
+					} */
+						
+							   		
+						
+					
+				}
 			}
 		}
+		
+	    collector.ack(input);
 	}
 
 	@Override
 	public void declareOutputFields(OutputFieldsDeclarer declarer) {
-	    declarer.declare(new Fields(Cons.TUPLE_VAR_MO, Cons.TUPLE_VAR_NOTIF, Cons.TUPLE_VAR_ADDTEXT, Cons.TUPLE_VAR_STATE));        
+	    declarer.declare(new Fields(Cons.TUPLE_VAR_MO, Cons.TUPLE_VAR_NOTIF, Cons.TUPLE_VAR_ADDTEXT, Cons.TUPLE_VAR_EVENTTIME, Cons.TUPLE_VAR_STATE));        
+	}
+	
+	private Integer generateNotifId(String field, String operation, String rule) {
+		
+		return (field+operation+rule).hashCode();
+	}
+	
+	private void generateActiveAlarm(String mo, Integer notif_id, String add_text, Long eventtime) {
+		
+		HashSet<Integer> s = active_notifs.get(mo);
+		
+		if (s == null) {
+			HashSet<Integer> notifs = new HashSet<Integer>();
+			notifs.add(notif_id);
+			this.collector.emit(new Values(mo, notif_id, add_text, eventtime, "active"));
+			active_notifs.put(mo, notifs);
+		}
+		else {
+			if (!s.contains(notif_id)) {
+				s.add(notif_id);
+				this.collector.emit(new Values(mo, notif_id, add_text, eventtime, "active"));
+			}
+			//If set of active notifs for this mo contains the specified id do nothing!
+		}
+	}
+	
+	private void generateClearAlarm(String mo, Integer notif_id, String add_text, Long eventtime) {
+		HashSet<Integer> s = active_notifs.get(mo);
+		
+		if (s != null) {
+			if (s.contains(notif_id)) {
+				this.collector.emit(new Values(mo, notif_id, add_text, eventtime));
+				s.remove(notif_id);  //Remove active
+			}
+		}
 	}
 
 }
